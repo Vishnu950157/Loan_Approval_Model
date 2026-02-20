82c9429bc1f08b46ba4bdb2ca60e75bcaae414d7
Vishnu Vardhan Rao <t.vishnuvardhanraovishnu@gmail.com>
2026-02-20 12:23:07 +0530

    Update func.js

--- static/func.js
+++ static/func.js
@@ -30,7 +30,7 @@ form.addEventListener("submit", async (e) => {
     console.log("Payload sent to API:", payload);
 
     try {
-        const response = await fetch("http://127.0.0.1:8000/predict", {
+        const response = await fetch("https://loan-approval-prediction-model-glos.onrender.com/", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(payload)
